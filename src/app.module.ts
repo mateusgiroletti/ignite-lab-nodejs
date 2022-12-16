7cc50048708e2ed1a334da406ed193d1768ef21f
Mateus Giroletti <mateusgiroletti97@gmail.com>
2022-12-16 16:03:34 -0300

    Adding notification class

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { HttpModule } from './http.module';
-import { PrismaService } from './prisma.service';
+import { AppController } from './infra/app.controller';
+import { HttpModule } from './infra/http.module';
+import { PrismaService } from './infra/prisma.service';
 
 @Module({
     imports: [HttpModule],
     controllers: [AppController],
     providers: [PrismaService],
 })
+
 export class AppModule { }
